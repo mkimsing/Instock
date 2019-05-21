c1fc6fa97e9bb8e87c9d28696b972e4ec280b285
LAPTOP-0PN2T9GL\MKS <mkimsing95@gmail.com>
2019-05-21 15:01:47 -0700

    Created server to use port 8080, added body parser, allowed cors

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//Ensure routes are defined before listening
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json()); //Body parser
+app.use(cors()); //Allow cross origin
+
+app.listen(PORT, () => {
+  console.log(`Server now listening on port ${PORT}...`);
+});
+
+//Put routes here:
+// app.use("/routePathHere", file);
