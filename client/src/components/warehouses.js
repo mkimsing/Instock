2545f3896388bc3e820fd3481b57c42fc472ab2e
carmour <callum.m.armour@gmail.com>
2019-05-21 17:31:27 -0700

    warehouses initial commit

---
+++ client/src/components/warehouses.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Warehouses() {
+    return(
+        <div>
+            <div>
+                <h1>Locations</h1>
+                <div>Search bar</div>
+            </div>
+            <div>
+                <div>WAREHOUSE</div>
+                <div>CONTACT</div>
+                <div>CONTACT INFORMATION</div>
+                <div>CATEGORIES</div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div>Warehouse name</div>
+                        <div>Warehouse address</div>
+                    </div>
+                    <div>arrow icon</div>
+                </div>
+                <div>
+                    <div>Contact name</div>
+                    <div>Contact position</div>
+                </div>
+                <div>
+                    <div>Contact number</div>
+                    <div>Contact email</div>
+                </div>
+                <div>
+                    <div>Description of services</div>
+                </div>
+            </div>
+        </div>
+    )
+}
