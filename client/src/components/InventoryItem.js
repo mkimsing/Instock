80eaaddeb5456946055275ec2c56b11ec6393987
LAPTOP-0PN2T9GL\MKS <mkimsing95@gmail.com>
2019-05-22 11:47:09 -0700

    Cleaned up kebab button and added remove button show/hide functionality

--- client/src/components/InventoryItem.js
+++ client/src/components/InventoryItem.js
@@ -1,33 +1,40 @@
 import React from "react";
-
-export default function InventoryItem() {
-  //TODO Make this dynamic using props for data
-  return (
-    // Mobile
-    <div className="item">
-      <div className="item__flexContainer">
-        <div className='kebabContainer'>
-          <h5 className='tableHeader'> ITEM </h5>
-          <button className='kebabButton'></button>
-        </div>
-        <div className="item__text">
-          <h2>Product Name Here</h2>
-          <h4>
-            Here is a very brief description of the product in the inventory
-          </h4>
-        </div>
-        <h5 className='tableHeader'>LAST ORDERED</h5>
-        <h4 className='tableCell'> 05/24/2018</h4>
-        <h5 className='tableHeader'>LOCATION</h5>
-        <h4 className='tableCell'> Toronto, CAN</h4>
-        <h5 className='tableHeader'> QUANTITY</h5>
-        <h4 className='tableCell'> 12,000</h4>
-        <h5 className='tableHeader'>STATUS</h5>
-        <div className='tableCell' id='kebabCell' >
-          <h4>In Stock</h4>
-          <button className='kebabButton fullSizeOnly'></button>
+import KebabButton from "./KebabButton";
+export default class InventoryItem extends React.Component {
+  state = {};
+  render() {
+    //TODO Make this dynamic using props for data
+    return (
+      // Mobile
+      <div className="item">
+        <div className="item__flexContainer">
+          <div className="topRow mobileOnly">
+            <h5 className="tableHeader"> ITEM </h5>
+            {/* <button className="kebabButton" />
+            <button className="removeButton"> Remove </button> */}
+            <KebabButton />
+          </div>
+          <div className="item__text">
+            <h2>Product Name Here</h2>
+            <h4>
+              Here is a very brief description of the product in the inventory
+            </h4>
+          </div>
+          <h5 className="tableHeader">LAST ORDERED</h5>
+          <h4 className="tableCell"> 05/24/2018</h4>
+          <h5 className="tableHeader">LOCATION</h5>
+          <h4 className="tableCell"> Toronto, CAN</h4>
+          <h5 className="tableHeader"> QUANTITY</h5>
+          <h4 className="tableCell"> 12,000</h4>
+          <h5 className="tableHeader">STATUS</h5>
+          <div className="tableCell" id="kebabCell">
+            <h4>In Stock</h4>
+            <div className="fullSizeOnly">
+              <KebabButton />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
---
+++ client/src/components/KebabButton.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class KebabButton extends Component {
+  state = {
+    removeButtonModifier: "removeButton--hide"
+  };
+
+  toggleRemoveButton = () => {
+    if (this.state.removeButtonModifier === "removeButton--hide") {
+      this.setState({
+        removeButtonModifier: "removeButton--show"
+      });
+    } else {
+      this.setState({
+        removeButtonModifier: "removeButton--hide"
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="kebabContainer">
+        <button className="kebabButton" onClick={this.toggleRemoveButton} />
+        <button className={`removeButton ${this.state.removeButtonModifier}`}>
+          Remove
+        </button>
+      </div>
+    );
+  }
+}
