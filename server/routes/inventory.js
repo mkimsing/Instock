dcab77df7e8eee4dbf0334dd772a6e7cfcb95626
webtech-pmp <webtech.pmp@gmail.com>
2019-05-22 15:47:06 -0700

    WIP inventory find item, 404 if not found

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -10,10 +10,20 @@ router
 
 //create an endpoint that will return an item based on its id.
 //If the id does not match, return a 404 status and an error message.
+router  
   .route('/inventory/:id')
-  .get((_req, res) => {
-
-  })
-
+    .get((_req, res) => {
+      const inventory = inventory.find(inventory => {
+        inventory.id === req.params.inventory;
+        if (inventory) {
+          res.json(inventory.filter(inventory =>
+            inventory.id === req.params.inventory));
+          } else {
+              res 
+                .status (404)
+                .json({errorMessage: `Inventory item with ID: ${req.params.id} -- ${req.params.item.name} not found`});
+          }
+        })
+      })
 
 module.exports = router; git 
