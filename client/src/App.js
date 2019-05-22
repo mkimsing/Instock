be8a642d409bf99684d268b08e08c66210261c00
LAPTOP-0PN2T9GL\MKS <mkimsing95@gmail.com>
2019-05-22 15:59:28 -0700

    Fixed syntax error with Client App

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./styles/App.css";
 import InventoryContainer from "./containers/InventoryContainer";
-import {Switch, Route} from "react-router-dom"
+import { Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
-render () {
-
-  return (
-    <div className="App">
-      <Switch>
-        {/* When a user hits the homepage, redirect them to the list of warehouses */}
-        <Route path="/" exact component={Warehouses} />
-        <Route path="/warehouses" component={Warehouses} />
-        <Route path="/inventory" component={InventoryContainer} />
-        <Route path="/warehouse/:id" component={Warehouse} />
-        <Route path="/inventory/:id" component={Inventory} />
-      </Switch>
-    </div>
-}
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          {/* When a user hits the homepage, redirect them to the list of warehouses */}
+          <Route path="/" exact component={Warehouse} />
+          <Route path="/warehouses" component={Warehouses} />
+          <Route path="/inventory" component={InventoryContainer} />
+          <Route path="/warehouse/:id" component={Warehouse} />
+          <Route path="/inventory/:id" component={Inventory} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
