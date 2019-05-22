acb0d6125a9a929bbb6bc8de8da3ecbaa73dd060
carmour <callum.m.armour@gmail.com>
2019-05-22 16:07:34 -0700

    warehouses_front

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./styles/App.css";
-import {Switch, Route} from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
+import WarehousesContainer from './containers/WarehousesContainer'
 
 class App extends React.Component {
-render () {
+  render() {
 
-  return (
-    <div className="App">
-      <Switch>
-        {/* When a user hits the homepage, redirect them to the list of warehouses */}
-        <Route path="/" exact component={Warehouses} />
-        <Route path="/warehouses" component={Warehouses} />
-        <Route path="/inventory" component={Inventory} />
-        <Route path="/warehouse/:id" component={Warehouse} />
-        <Route path="/inventory/:id" component={Inventory} />
-      </Switch>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Switch>
+          {/* When a user hits the homepage, redirect them to the list of warehouses */}
+          <Route path="/" exact component={Warehouses} />
+          <Route path="/warehouses" component={WarehousesContainer} />
+          <Route path="/inventory" component={Inventory} />
+          <Route path="/warehouse/:id" component={Warehouse} />
+          <Route path="/inventory/:id" component={Inventory} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/containers/WarehousesContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class WarehousesContainer extends Component {
+    state = {
+        warehouses: {}
+    }
+    componentDidMount() {
+        axios.get('http://localhost:8080/warehouses')
+            .then(response => {
+                console.log(response.data)
+                this.setState({
+                    warehouses: response.data
+                })
+            })
+    }
+    render() {
+        return (
+            <div>aksdjf</div>
+        )
+    }
+
+}
+
+export default WarehousesContainer;
