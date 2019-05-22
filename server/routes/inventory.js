b6b901563936bbb4fd67b8e772b5038864918218
webtech-pmp <webtech.pmp@gmail.com>
2019-05-22 16:46:13 -0700

    fixed find item

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -11,20 +11,20 @@ router
 //Endpoint that will return an item based on its id.
 //If the id does not match, return a 404 status and an error message.
 router  
-  .route('/inventory/:id')
+  .route('/:id')
     .get((req, res) => {
-      const inventory = inventory.find(inventory => {
-        inventory.id === req.params.inventory;
+      const inventoryData =inventoryController.getAllInventory();
+      const inventory = inventoryData.find(inventory => {
+        return inventory.id === req.params.id;})
         if (inventory) {
-          res.json(inventory.filter(inventory =>
-            inventory.id === req.params.inventory));
+          res.json(inventory)
           } else {
               res 
                 .status (404)
-                .json({errorMessage: `Inventory item with ID: ${req.params.id} -- ${req.params.item.name} not found`});
+                .json({errorMessage: `Inventory item with ID: ${req.params.id} not found`});
           }
         })
-      })
+      
 
 
 
