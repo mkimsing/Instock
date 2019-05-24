415dc07264122a63f9fdf8ec7d3d177a4b48fdcc
webtech-pmp <webtech.pmp@gmail.com>
2019-05-23 17:02:27 -0700

    delete-item

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -10,41 +10,40 @@ router
 
 //Endpoint that will return an item based on its id.
 //If the id does not match, return a 404 status and an error message.
-router  
-  .route('/:id')
-    .get((req, res) => {
-      const inventoryData =inventoryController.getAllInventory();
-      const inventory = inventoryData.find(inventory => {
-        return inventory.id === req.params.id;})
-        if (inventory) {
-          res.json(inventory)
-          } else {
-              res 
-                .status (404)
-                .json({errorMessage: `Inventory item with ID: ${req.params.id} not found`});
-          }
-        })
-      
-//Endpoint that will delete an item specified by id.
-router
-  .delete('/inventory/:id', (req, res) => {
-    const found = inventory.some(
-      inventory => inventory.id === parseInt(req.params.id)
-    );
-    if (found) {
-      const inventoryAfterDeletion = inventory.filter(
-        inventory => inventory.id !== parseInt(req.params.id)
-      );
-      helper.writeJSONfile(fileName, inventoryAfterDeletion);
-      res.json({
-        msg: `Inventory item with ID: ${req.params.id} -- ${req.params.item[name]} has been deleted`,
-        inventory: inventoryAfterDeletion
-      });
-    } else {
-      res
-        .status(404)
-        .json({ errorMessage: `Inventory item with ID: ${req.params.id} not found` })
-    }
-  })
+router.route("/:id").get((req, res) => {
+  const inventoryData = inventoryController.getAllInventory();
+  const inventory = inventoryData.find(inventory => {
+    return inventory.id === req.params.id;
+  });
+  if (inventory) {
+    res.json(inventory);
+  } else {
+    res.status(404).json({
+      errorMessage: `Inventory item with ID: ${req.params.id} not found`
+    });
+  }
+});
 
-module.exports = router; 
+//Endpoint that will delete an item specified by id.
+router.delete("/inventory/:id", (req, res) => {
+  const inventory = inventoryController.getAllInventory();
+  const found = inventory.some(
+    inventory => inventory.id === parseInt(req.params.id)
+  );
+  if (found) {
+    const inventoryAfterDeletion = inventory.filter(
+      inventory => inventory.id !== parseInt(req.params.id)
+    );
+    helper.writeJSONfile(fileName, inventoryAfterDeletion);
+    res.json({
+      msg: `Inventory item with ID: ${req.params.id} has been deleted`,
+      inventory: inventoryAfterDeletion
+    });
+  } else {
+    res.status(404).json({
+      errorMessage: `Inventory item with ID: ${req.params.id} not found`
+    });
+  }
+});
+
+module.exports = router;
