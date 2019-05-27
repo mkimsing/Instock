169c699e5f2feaf11ae5e5fc3ef409fcfd619421
Sampson Mui <trendzme@gmail.com>
2019-05-27 14:36:58 -0700

    completed product detail page

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,24 @@ import "./styles/App.css";
 import { Switch, Route } from "react-router-dom";
 import WarehousesContainer from "./containers/WarehousesContainer";
 import InventoryContainer from "./containers/InventoryContainer";
+import ProductDetailComponent from "./components/ProductDetailComponent";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Switch>
+        <ProductDetailComponent />
+        {/* <Switch>
           {/* When a user hits the homepage, redirect them to the list of warehouses */}
-
-          <Route path="/" exact component={WarehousesContainer} />
+        {/* <Route path="/" exact component={WarehousesContainer} />
           <Route path="/warehouses" component={WarehousesContainer} />
-          <Route path="/inventory" component={InventoryContainer} />
-          {/* <Route path="/warehouse/:id" component={Warehouse} /> */}
-          {/* <Route path="/inventory/:id" component={Inventory} /> */}
-        </Switch>
+          <Route path="/inventory" component={InventoryContainer} /> */}
+        {/* <Route path="/warehouse/:id" component={Warehouse} /> */}
+        {/* <Route path="/inventory/:id" component={Inventory} /> */}
+        {/* </Switch> */}
       </div>
     );
   }
-
 }
 
 export default App;
