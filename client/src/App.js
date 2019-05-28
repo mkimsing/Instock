e9be7cc88a2f50d21c87192d1ecfc5b091fc9f55
Sampson Mui <trendzme@gmail.com>
2019-05-27 17:58:19 -0700

    routing detail and edit page

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,27 @@ import { Switch, Route } from "react-router-dom";
 import WarehousesContainer from "./containers/WarehousesContainer";
 import InventoryContainer from "./containers/InventoryContainer";
 
-import ProductDetailComponent from "./components/ProductDetailComponent";
-import WarehouseInventoryContainer from "./containers/WarehouseInventoryContainer"
+import WarehouseInventoryContainer from "./containers/WarehouseInventoryContainer";
+import ProductContainer from "./containers/ProductContainer";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <ProductDetailComponent />
         <Switch>
           {/* When a user hits the homepage, redirect them to the list of warehouses */}
 
           <Route path="/" exact component={WarehousesContainer} />
           <Route path="/warehouses" exact component={WarehousesContainer} />
-          <Route path="/warehouses/:warehouseID" exact component={WarehouseInventoryContainer} />
+          <Route
+            path="/warehouses/:warehouseID"
+            exact
+            component={WarehouseInventoryContainer}
+          />
 
-          <Route path="/inventory" component={InventoryContainer} />
-          {/* <Route path="/warehouse/:id" component={Warehouse} /> */}
-          {/* <Route path="/inventory/:id" component={Inventory} /> */}
+          <Route path="/inventory" exact component={InventoryContainer} />
+          <Route path="/inventory/:id" component={ProductContainer} />
         </Switch>
-
       </div>
     );
   }
