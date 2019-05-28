81fb5664b27e47ef2b18feb086dc74f91d7e6379
webtech-pmp <webtech.pmp@gmail.com>
2019-05-28 07:11:04 -0700

    add warehouse mobile ok,tablet,desktop WIP

--- client/src/components/AddWarehouse.js
+++ client/src/components/AddWarehouse.js
@@ -7,18 +7,18 @@ export default class AddWarehouse extends Component {
       <div className="addWarehouse">
         <h1>Add New</h1>
         <form>
-          <div className="addWarehouse__header caps">
-            Warehouse
+          <>
+            <div className="addWarehouse__header">Warehouse</div>
             <input
               className="addWarehouse__input"
               type="text"
               name="warehouse"
               placeholder="Name"
             />
-          </div>
+          </>
           <div className="addWarehouse__address-location">
-            <div className="addWarehouse__header caps">
-              Address
+            <div className="addWarehouse__stack">
+              <div className="addWarehouse__header">Address</div>
               <input
                 className="addWarehouse__input"
                 type="text"
@@ -26,8 +26,8 @@ export default class AddWarehouse extends Component {
                 placeholder="Enter Address"
               />
             </div>
-            <div className="addWarehouse__header caps">
-              Location
+            <div className="addWarehouse__stack">
+              <div className="addWarehouse__header">Location</div>
               <select
                 className="addWarehouse__input addWarehouse__input--select"
                 name="city"
@@ -38,57 +38,55 @@ export default class AddWarehouse extends Component {
             </div>
           </div>
           <div className="addWarehouse__contact-position">
-            <div className="addWarehouse__header caps">
-              Contact Name
-              <input
+            <div className="addWarehouse__stack">
+                <div className="addWarehouse__header">Contact Name</div>
+                <input
                 className="addWarehouse__input"
                 type="text"
                 name="contact"
                 placeholder="Enter Name"
-              />
+                />
             </div>
-            <div className="addWarehouse__header caps">
-              Position
-              <input
+            <div className="addWarehouse__stack">
+                <div className="addWarehouse__header">Position</div>
+                <input
                 className="addWarehouse__input"
                 type="text"
                 name="position"
                 placeholder="Enter Position"
-              />
+                />
             </div>
           </div>
           <div className="addWarehouse__contact-info">
-            <div className="addWarehouse__header caps">
-              Phone Number
-              <input
+            <div className="addWarehouse__stack"> 
+                <div className="addWarehouse__header">Phone Number</div>
+                <input
                 className="addWarehouse__input"
                 type="text"
                 name="phone"
                 placeholder="(000) 000 - 0000"
-              />
+                />
             </div>
-          </div>
-          <div className="addWarehouse__header caps">
-            Email
-            <input
-              className="addWarehouse__input"
-              type="text"
-              name="email"
-              placeholder="email@instock.com"
-            />
-          </div>
+            <div className="addWarehouse__stack">
+                <div className="addWarehouse__header">Email</div>
+                <input
+                className="addWarehouse__input"
+                type="text"
+                name="email"
+                placeholder="email@instock.com"
+                />
+            </div>
+           </div>
           <div className="addWarehouse__categories">
-            <div className="addWarehouse__header caps">
-              Categories
-              <textarea
-                className="addWarehouse__input addWarehouse__input--description"
-                placeholder="Use commas to separate each category"
-              />
-            </div>
+            <div className="addWarehouse__header">Categories</div>
+            <textarea
+              className="addWarehouse__input addWarehouse__input--description"
+              placeholder="Use commas to separate each category"
+            />
           </div>
           <div className="addWarehouse__buttons">
             <button>SAVE</button>
-            <button>CANCEL</button>
+            <button className="cancel-button">CANCEL</button>
           </div>
         </form>
       </div>
