1895cd1978c991167371e8703ffe0d7f23f67a45
LAPTOP-0PN2T9GL\MKS <mkimsing95@gmail.com>
2019-05-28 16:53:05 -0700

    Added functionality skeleton for adding a warehouse
    *Untested -> will test thoroughly when page front-end/view is complete

--- client/src/components/AddWarehouse.js
+++ client/src/components/AddWarehouse.js
@@ -1,12 +1,69 @@
 import React, { Component } from "react";
 import Switch from "react-switch";
-
+import helper from "../helpers/helper";
 export default class AddWarehouse extends Component {
+  submitHandler = event => {
+    event.preventDefault();
+    let {
+      warehouse,
+      address,
+      // address2,
+      // postalCode,
+      city,
+      contact, //Name
+      position,
+      phone,
+      email,
+      categories
+    } = event.target;
+    let fields = [
+      warehouse,
+      address,
+      // address2,
+      // postalCode,
+      city,
+      contact, //Name
+      position,
+      phone,
+      email,
+      categories
+    ];
+    let allFilled = fields
+      .map(field => {
+        return helper.isEmpty(field);
+      })
+      .every(val => {
+        return val === false;
+      });
+    if (allFilled) {
+      console.log("all filled!");
+      const locationObj = {
+        address1: address.value,
+        // address2: address2.value,
+        region: city.value
+        // postalCode: `${postalCode.value}, CA`
+      };
+
+      const contactObj = {
+        name: contact.value,
+        position: position.value,
+        phone: phone.value,
+        email: email.value
+      };
+
+      const categoriesArr = categories.value.split(",");
+      console.log(locationObj, contactObj, categoriesArr);
+      // this.props.postNewWarehouse(warehouse.value, locationObj, contactObj, categoriesArr);
+    } else {
+      console.log("At least one is empty");
+    }
+  };
+
   render() {
     return (
       <div className="addWarehouse">
         <h1>Add New</h1>
-        <form>
+        <form onSubmit={this.submitHandler}>
           <>
             <div className="addWarehouse__header">Warehouse</div>
             <input
@@ -40,49 +97,50 @@ export default class AddWarehouse extends Component {
           </div>
           <div className="addWarehouse__contact-position">
             <div className="addWarehouse__stack">
-                <div className="addWarehouse__header">Contact Name</div>
-                <input
+              <div className="addWarehouse__header">Contact Name</div>
+              <input
                 className="addWarehouse__input"
                 type="text"
                 name="contact"
                 placeholder="Enter Name"
-                />
+              />
             </div>
             <div className="addWarehouse__stack">
-                <div className="addWarehouse__header">Position</div>
-                <input
+              <div className="addWarehouse__header">Position</div>
+              <input
                 className="addWarehouse__input"
                 type="text"
                 name="position"
                 placeholder="Enter Position"
-                />
+              />
             </div>
           </div>
           <div className="addWarehouse__contact-info">
-            <div className="addWarehouse__stack"> 
-                <div className="addWarehouse__header">Phone Number</div>
-                <input
+            <div className="addWarehouse__stack">
+              <div className="addWarehouse__header">Phone Number</div>
+              <input
                 className="addWarehouse__input"
                 type="text"
                 name="phone"
                 placeholder="(000) 000 - 0000"
-                />
+              />
             </div>
             <div className="addWarehouse__stack">
-                <div className="addWarehouse__header">Email</div>
-                <input
+              <div className="addWarehouse__header">Email</div>
+              <input
                 className="addWarehouse__input"
                 type="text"
                 name="email"
                 placeholder="email@instock.com"
-                />
+              />
             </div>
-           </div>
+          </div>
           <div className="addWarehouse__categories">
             <div className="addWarehouse__header">Categories</div>
             <textarea
               className="addWarehouse__input addWarehouse__input--description"
               placeholder="Use commas to separate each category"
+              name="categories"
             />
           </div>
           <div className="addWarehouse__buttons">
